0b427ddc25868e0fb418893eb9544bf6c1706942
alessandro-sartini <ale.sartini.98@gmail.com>
2025-11-21 16:49:22 +0100

    Implement category selection and pagination in product cards

--- src/app/features/product/sub-category-selection/sub-category-selection.ts
+++ src/app/features/product/sub-category-selection/sub-category-selection.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Category } from '../../../shared/models/Category ';
 
 @Component({
   selector: 'app-sub-category-selection',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sub-category-selection.html',
   styleUrl: './sub-category-selection.css',
 })
 export class SubCategorySelection {
+  @Input() categories: Category[] = [];
+  @Input() selectedCategoryId: number | null = null;
+  @Output() categoryChange = new EventEmitter<number | null>();
 
+  selectCategory(categoryId: number | null): void {
+    this.categoryChange.emit(categoryId);
+  }
+  showAll = false;
+
+  // =categorie ordinate per numero prodotti
+  get sortedCategories(): Category[] {
+    return [...this.categories].sort((a, b) => b.productCount - a.productCount);
+  }
+
+  // categorie da visualizzare=
+  get visibleCategories(): Category[] {
+    return this.showAll ? this.sortedCategories : this.sortedCategories.slice(0, 5);
+  }
+
+  //  ci sono altre categorie da mostrare?
+  get hasMoreCategories(): boolean {
+    return this.categories.length > 5;
+  }
+
+  // mostra tutto
+  toggleShowAll(): void {
+    this.showAll = !this.showAll;
+  }
 }
