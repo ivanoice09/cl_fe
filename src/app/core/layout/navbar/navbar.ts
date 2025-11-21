fd4d03ce58ae597917169aa3b84f89059fba6219
Ericson Manalo <ericmana98@gmail.com>
2025-11-21 14:18:14 +0100

    refactor navbar: integrate MainCategoriesBar and enhance menu functionality

--- src/app/core/layout/navbar/navbar.ts
+++ src/app/core/layout/navbar/navbar.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import {MainCategoriesBar} from './main-categories-bar/main-categories-bar';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MainCategoriesBar, RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  showHamburgerMenu = false;
+  childLinks = [
+    { label: 'BIKES', path: '/bikes' },
+    { label: 'COMPONENT', path: '/component' },
+    { label: 'CLOTHING', path: '/clothing' },
+    { label: 'ACCESSORIES', path: '/accessories' }
+  ];
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    if (window.innerWidth > 727 && this.showHamburgerMenu) {
+      this.showHamburgerMenu = false;
+    }
+  }
+
+    toggleMenu() {
+    this.showHamburgerMenu = !this.showHamburgerMenu;
+  }
 
 }
