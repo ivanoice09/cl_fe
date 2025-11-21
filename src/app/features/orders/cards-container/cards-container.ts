286e4388cd790c212a529c1c599044c47dce6d93
ivanoice09 <ivankovicsibsion@gmail.com>
2025-11-21 15:55:20 +0100

    Creato la cartella folder e i componenti cards-container e cards all'interno

---
+++ src/app/features/orders/cards-container/cards-container.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-container',
+  imports: [],
+  templateUrl: './cards-container.html',
+  styleUrl: './cards-container.css',
+})
+export class CardsContainer {
+
+}
