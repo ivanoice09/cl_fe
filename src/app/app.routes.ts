1b22e506e64cf64b89f57e8b7408f32e11e81059
ivanoice09 <ivankovicsibsion@gmail.com>
2025-11-23 01:44:01 +0100

    aggiunta della rotta per portare alla home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CardsContainer } from './features/product/cards-container/cards-container';
 import { Detail } from './features/product/cards-container/card/detail/detail';
 import { Login } from './features/auth/login/login';
+import { Home } from './features/home/home';
 
 export const routes: Routes = [
   { path: '', component: CardsContainer },
   { path: 'product/:id', component: Detail },
   { path: 'login', component: Login, data: { hideNavbar: true } },
+  { path: 'home', component: Home }
 ];
