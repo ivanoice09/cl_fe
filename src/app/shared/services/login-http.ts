320a8a73fc866cfe507d1dce49c6a0e521be60e2
ivanoice09 <ivankovicsibsion@gmail.com>
2025-11-23 01:47:50 +0100

    Chiamata endpoint per il login

---
+++ src/app/shared/services/login-http.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginCredentials } from '../models/LoginCredentials';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginHttp {
+  
+  constructor(private http: HttpClient) {}
+
+  HttpPostLogin(loginCredentials: LoginCredentials): Observable<any> {
+    return this.http.post('https://localhost:7000/api/Auth/Login', loginCredentials, { observe: 'response', withCredentials: true})
+  } 
+}
