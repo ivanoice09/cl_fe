4e230b02b20fe43df4641c20dc2da2378e58374d
alessandro-sartini <ale.sartini.98@gmail.com>
2025-11-24 17:05:48 +0100

    FIX card click

--- src/app/features/product/cards-container/card/card.ts
+++ src/app/features/product/cards-container/card/card.ts
@@ -11,10 +11,15 @@ import { Router } from '@angular/router';
 export class Card {
   @Input() product!: ProductCard;
 
-
   constructor(private router: Router) {}
 
-  onSelect() {
-    this.router.navigate(['/product', this.product.productId]);
+  onSelect(event: MouseEvent) {
+    const selection = window.getSelection();
+    // Se c'è del testo selezionato e la lunghezza > 0, NON navigare!
+    if (selection && selection.toString().length > 0) {
+      return; // Permetti selezione/copia, nessuna navigazione
+    }
+    // Altrimenti, esegui la navigazione
+    this.router.navigate(['product', this.product.productId]);
   }
 }
