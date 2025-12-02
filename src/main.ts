d884b753fa80c1e808fc69808caeb6b588c9c3bf
Ivan-da-casa <ivankovicsibsion@gmail.com>
2025-12-02 18:13:47 +0100

    clean version of applying the HTTPInterceptor

--- src/main.ts
+++ src/main.ts
@@ -3,28 +3,11 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { App } from './app/app';
 import { routes } from './app/app.routes';
-import { jwtInterceptor } from './app/core/interceptors/jwt.interceptor';
+import { authInterceptor } from './app/core/interceptors/auth-interceptor';
 
 bootstrapApplication(App, {
   providers: [
-    // provideHttpClient(
-    //   withInterceptors([
-    //     (req, next) => {
-    //       const token = localStorage.getItem('jwtToken') || sessionStorage.getItem('jwtToken');
-
-    //       if (token) {
-    //         req = req.clone({
-    //           setHeaders: {
-    //             Authorization: `Bearer ${token}`
-    //           }
-    //         });
-    //       }
-
-    //       return next(req);
-    //     }
-    //   ])
-    // ),
-    provideHttpClient(withInterceptors([jwtInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(routes)
   ]
 }).catch((err) => console.error(err));
