e566edb4dbc7b5f1291d440e4b463b9042fff7b1
alessandro-sartini <ale.sartini.98@gmail.com>
2025-12-11 15:58:18 +0100

    ADD 404-page logic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,32 +7,31 @@ import { Profile } from './features/profile/profile';
 import { ResetPassword } from './features/auth/reset-password/reset-password';
 import { OrderDetailComponent } from './features/orders/order-detail/order-detail';
 import { Register } from './features/auth/register/register';
-import { Addresses } from './features/profile/addresses/addresses';
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
   { path: 'products', component: CardsContainer },
   { path: 'product/:id', component: Detail },
+  
   { path: 'login', component: Login, data: { hideNavbar: true } },
   { path: 'reset-password', component: ResetPassword, data: { hideNavbar: true } },
-  { path: 'register', component: Register, data: {hideNavbar: true }},
-  { path: 'home', component: Home },
-  { path: 'profile', component: Profile },
-  { path: "orders/:orderId", component: OrderDetailComponent },
+  { path: 'register', component: Register, data: { hideNavbar: true } },
+
+  { path: 'orders/:orderId', component: OrderDetailComponent },
 
   { 
     path: 'profile', 
     component: Profile,
     children: [
       {
-        path: 'addresses', // Questa ora corrisponde a /profile/addresses
-        loadComponent: () =>
-          import('./features/profile/addresses/addresses').then(m => m.Addresses)
-      },
-      // Qui potresti aggiungere altre rotte figlie del profilo
-      // { path: '', redirectTo: 'dashboard', pathMatch: 'full' } // Ad esempio, reindirizzare a una dashboard di default
+        path: 'addresses', 
+        loadComponent: () => import('./features/profile/addresses/addresses').then(m => m.Addresses)
+      }
     ]
-  }
+  },
 
-  //{ path: 'reset-password', component: ResetPassword, data: { hideNavbar: true } }
-  
+  { path: '404', component: NotFound }, 
+  { path: '**', component: NotFound }
 ];
