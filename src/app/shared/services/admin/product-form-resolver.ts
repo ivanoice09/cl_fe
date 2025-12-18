bb67f1f1f07a36b529d0d2d6bb47513d5c475d8f
ivanoice09 <ivankovicsibsion@gmail.com>
2025-12-18 14:54:44 +0100

    made a resolver for preloading data

---
+++ src/app/shared/services/admin/product-form-resolver.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { AdminProductHttp } from './admin-product-http';
+import { ReferenceData } from './reference-data';
+import { forkJoin, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductFormResolver implements Resolve<any> {
+
+  constructor(
+    private adminProductHttp: AdminProductHttp,
+    private referenceData: ReferenceData
+  ) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const id = route.paramMap.get('id');
+
+    return forkJoin({
+      categories: this.referenceData.getCategories(),
+      models: this.referenceData.getModels(),
+      product: id ? this.adminProductHttp.getProduct(+id) : of(null)
+    });
+  }
+}
